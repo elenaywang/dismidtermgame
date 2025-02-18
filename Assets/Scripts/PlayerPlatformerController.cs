d1d3daa57f682fb3d6c7bf10ea0d92ac8be64c69
Elena Wang <ewang88@smith.edu>
2025-02-18 03:01:21 +0100

    started basic player controller for platformer. still needs troubleshooting

---
+++ Assets/Scripts/PlayerPlatformerController.cs
@@ -0,0 +1,68 @@
+// using System.Numerics;
+using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+public class PlayerPlatformerController : MonoBehaviour
+{
+    public float speed = 5;
+    public float jumpForce = 3;
+
+    [Header("Grounding")]
+    public LayerMask groundMask;
+    public float groundRayLength = 0.1f;
+    public float groundRaySpread = 0.4f;
+    public bool grounded = false;
+
+    private Rigidbody2D rb2d;
+
+
+    // NOTE: if it's not already there... don't forget to add Physics Material 2D to Assets, and change friction to 0.
+    // Then change the player sprite --> rigidbody2d --> material to the NoFriction material you just made.
+
+
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        rb2d = GetComponent<Rigidbody2D>();
+    }
+
+
+    // Update is called once per frame
+    void Update()
+    {
+        Vector2 vel = rb2d.linearVelocity;
+        vel.x = Input.GetAxis("Horizontal") * speed;
+
+        UpdateGrounding();
+
+        bool inputJump = Input.GetKeyDown(KeyCode.Space);
+        if (inputJump && grounded) {
+            vel.y = jumpForce;
+        }
+
+        rb2d.linearVelocity = vel;
+    }
+
+
+    void UpdateGrounding() {
+        Vector3 rayStart = transform.position + Vector3.up * groundRayLength;
+        Vector3 rayStartLeft = transform.position + Vector3.up * groundRayLength + Vector3.left * groundRaySpread;
+        Vector3 rayStartRight = transform.position + Vector3.up * groundRayLength + Vector3.right * groundRaySpread;
+
+        RaycastHit2D hit = Physics2D.Raycast(rayStart, Vector3.down, groundRayLength * 2, groundMask);
+        RaycastHit2D hitLeft = Physics2D.Raycast(rayStartLeft, Vector3.down, groundRayLength * 2, groundMask);
+        RaycastHit2D hitRight = Physics2D.Raycast(rayStartRight, Vector3.down, groundRayLength * 2, groundMask);
+
+        Debug.DrawLine(rayStart, rayStart + Vector3.down * groundRayLength * 2, Color.red);
+        Debug.DrawLine(rayStartLeft, rayStartLeft + Vector3.down * groundRayLength * 2, Color.red);
+        Debug.DrawLine(rayStartRight, rayStartRight + Vector3.down * groundRayLength * 2, Color.red);
+
+
+        if (hit.collider != null || hitLeft.collider != null || hitRight.collider != null) {
+            grounded = true; 
+        } else {
+            grounded = false;
+        }
+    }
+}
