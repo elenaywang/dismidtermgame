b100cb48c0fa9269f452637679e17119e17f9d31
Sarah Than <sthan@colgate.edu>
2025-02-18 12:33:52 +0100

    added multiplier buttons

---
+++ Assets/Scripts/Multiply3.cs
@@ -0,0 +1,10 @@
+using UnityEngine;
+
+public class Multiply3 : MonoBehaviour
+{
+    public GameObject textBox;
+
+    public void clickTheButton(){
+        IncreaseThreshold.thresholdCount += 5;
+    }
+}
