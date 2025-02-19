269876b769e7e47fb0130fc1b298a82dcd22c9ad
Elena Wang <ewang88@smith.edu>
2025-02-19 13:32:43 +0100

    added object management and door script

---
+++ Assets/Scripts/Door.cs
@@ -0,0 +1,28 @@
+using UnityEngine;
+
+public class Door : MonoBehaviour
+{
+
+    bool locked = true;
+
+
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        
+    }
+
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+
+    public void Unlock() {
+        locked = false;
+        Debug.Log("door is unlocked");
+        Destroy(gameObject);
+    }
+}
---
+++ Assets/Scripts/ObjectManager.cs
@@ -0,0 +1,42 @@
+using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+public class ObjectManager : MonoBehaviour
+{
+
+    GameObject obj1;
+    GameObject obj2;
+    GameObject obj3;
+
+
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        
+    }
+
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+
+	void OnTriggerEnter2D(Collider2D other) {
+		if(other.CompareTag("Obj1") && obj1 == null) {
+            obj1 = other.gameObject;
+            Debug.Log("you collected obj1!");
+            obj1.GetComponent<Object>().doorToUnlock.Unlock();      // open door to section 2
+		} else if(other.CompareTag("Obj2") && obj2 == null) {
+            obj2 = other.gameObject;
+            Debug.Log("you collected obj2!");
+            obj2.GetComponent<Object>().doorToUnlock.Unlock();      // open door to section 3
+		} else if(other.CompareTag("Obj3") && obj3 == null) {
+            obj3 = other.gameObject;
+            Debug.Log("you collected obj3!");
+            obj3.GetComponent<Object>().doorToUnlock.Unlock();      // open last door
+		} 
+	}
+}
