1edab33c7a95a4cc467ff8b50ecf227e4b2b049d
Elena Wang <ewang88@smith.edu>
2025-02-26 17:08:12 +0100

    implemented clicker and platformer switch yay!

--- Assets/Scripts/ObjectManager.cs
+++ Assets/Scripts/ObjectManager.cs
@@ -14,45 +14,30 @@ public class ObjectManager : MonoBehaviour
     GameObject obj2;
     GameObject obj3;
 
-
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
-    void Start()
-    {
-        
-    }
-
-
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
+    public GameObject thresholdDisplay;
 
 
 	void OnTriggerEnter2D(Collider2D other) {
 		if(other.CompareTag("Obj1") && obj1 == null) {
             obj1 = other.gameObject;
             Debug.Log("you collected obj1!");
-            obj1.GetComponent<Object>().doorToUnlock.Unlock();      // open door to section 2
+            obj1.GetComponent<Object>().doorToUnlock.Unlock();                          // open door to section 2
             GetComponent<PlayerPlatformerController>().section = 2;
-            // switch to clicker game - CanvasObject.GetComponent<Canvas>().enabled = true;
-            //IncreaseThreshold.clickCanvas.enabled = true;
+            thresholdDisplay.GetComponent<IncreaseThreshold>().switchToClicker();       // switch to clicker game
 		} else if(other.CompareTag("Obj2") && obj2 == null) {
             obj2 = other.gameObject;
             Debug.Log("you collected obj2!");
-            obj2.GetComponent<Object>().doorToUnlock.Unlock();      // open door to section 3
+            obj2.GetComponent<Object>().doorToUnlock.Unlock();                          // open door to section 3
             GetComponent<PlayerPlatformerController>().section = 3;
-            // switch to clicker game
-            //IncreaseThreshold.clickCanvas.enabled = true;
+            thresholdDisplay.GetComponent<IncreaseThreshold>().switchToClicker();       // switch to clicker game
 		} else if(other.CompareTag("Obj3") && obj3 == null) {
             obj3 = other.gameObject;
             Debug.Log("you collected obj3!");
-            obj3.GetComponent<Object>().doorToUnlock.Unlock();      // open last door
+            obj3.GetComponent<Object>().doorToUnlock.Unlock();                          // open last door
             GetComponent<PlayerPlatformerController>().section = 4;
-            // switch to clicker game
-            //IncreaseThreshold.clickCanvas.enabled = true;
+            thresholdDisplay.GetComponent<IncreaseThreshold>().switchToClicker();       // switch to clicker game
 		} else if (other.CompareTag("Trap")) {
-            GetComponent<PlayerPlatformerController>().hp -= 5;     // decrement health points
+            GetComponent<PlayerPlatformerController>().hp -= 5;                         // decrement health points
             Debug.Log(String.Format("you fell into a trap :( \t hp = {0}", GetComponent<PlayerPlatformerController>().hp));
         }
 	}
